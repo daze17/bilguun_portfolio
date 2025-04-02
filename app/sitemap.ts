b0d2d86477775328ed06d0adb319967ca5c4efff
daze <bilguun889@gmail.com>
2025-04-02 16:26:29 +0800

    fix: rss and sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,16 @@
 import { getBlogPosts } from "@/[locale]/blog/utils";
+import { getWorkPosts } from "@/[locale]/work/utils";
 
 export const baseUrl = "https://bilguun-portfolio.vercel.app";
 
 export default async function sitemap() {
   let blogs = getBlogPosts("en").map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
+    url: `${baseUrl}/en/blog/${post.slug}`,
+    lastModified: post.metadata.publishedAt,
+  }));
+
+  let works = getWorkPosts("en").map((post) => ({
+    url: `${baseUrl}/en/work/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }));
 
@@ -13,5 +19,5 @@ export default async function sitemap() {
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...blogs];
+  return [...routes, ...blogs, ...works];
 }
