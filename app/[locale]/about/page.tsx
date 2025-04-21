3b51fc85143b2664e32186e42418153f3bfd9b1c
daze <bilguun889@gmail.com>
2025-04-21 23:52:31 +0800

    chore

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -3,7 +3,8 @@ import { getTranslations } from "next-intl/server";
 
 import { cn } from "@/utils";
 
-import AboutMeLayout from "./about_me_layout";
+import FavMoviesSection from "./movies";
+import SpotifySection from "./music";
 import SkillSection from "./skills";
 
 const AboutPage: React.Page = async () => {
@@ -20,12 +21,11 @@ const AboutPage: React.Page = async () => {
       </h1>
       <div className="grid md:grid-cols-2 gap-20 items-start my-20">
         <Image
-          // TODO: replace the goddamn image
-          src={"/album/cafe.jpeg"}
+          src={"/bilguun_pfp.jpg"}
           alt={"my-image"}
           width={500}
           height={200}
-          className="object-contain"
+          className="rounded-lg object-contain"
         />
 
         <div className="flex flex-col justify-center h-full">
@@ -36,8 +36,7 @@ const AboutPage: React.Page = async () => {
         </div>
       </div>
       <SkillSection />
-
-      {/* <h1
+      <h1
         className={cn(
           "text-5xl md:text-8xl",
           "font-semibold mt-52 mb-12 tracking-tighter text-center",
@@ -45,8 +44,24 @@ const AboutPage: React.Page = async () => {
       >
         {t("about.more")}
       </h1>
-
-      <AboutMeLayout /> */}
+      {/* TODO: make component */}
+      <div className="grid md:grid-cols-3 gap-5 items-start my-20">
+        <Image
+          src={"/hiking_2.jpeg"}
+          alt={"hiking"}
+          width={400}
+          height={200}
+          className="rounded-lg object-contain"
+        />
+        <div className="flex flex-col justify-center h-full md:col-span-2">
+          <p>
+            Outside of coding, I love traveling, hiking, experimenting in the
+            kitchen, reading manga and watching anime, and gaming.
+          </p>
+        </div>
+      </div>
+      <FavMoviesSection />
+      <SpotifySection />
     </section>
   );
 };
